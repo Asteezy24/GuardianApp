bd15c0293a642a96b2259033acc9529b5e125a11
Asteezy24 <alxtv1026@gmail.com>
2019-10-21 10:17:47 -0400

    initial commit

---
+++ GuardianApp/SearchStudent/SearchStudentViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchStudentViewController.swift
+//  GuardianApp
+//
+//  Created by Alexander Stevens on 10/21/19.
+//  Copyright © 2019 Alex Stevens. All rights reserved.
+//
+
+import UIKit
+
+class SearchStudentViewController: UIViewController {
+    
+}
---
+++ GuardianApp/StudentGuardianSelection/StudentGuardianSelectionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  StudentGuardianSelectionViewController.swift
+//  GuardianApp
+//
+//  Created by Alexander Stevens on 10/21/19.
+//  Copyright © 2019 Alex Stevens. All rights reserved.
+//
+
+import UIKit
+
+class StudentGuardianSelectionViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
